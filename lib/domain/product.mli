0b09d0f1712fdf93587dd97fd39eb9f7758b2d5e
nguyenhoangan-monkey <an.h.nguyen@stonybrook.edu>
2026-05-21 16:26:56 -0400

    product.mli and product.ml rewrite

--- lib/domain/product.mli
+++ lib/domain/product.mli
@@ -1,27 +1,31 @@
 (* See README for instructions and type definitions *)
 
+(** Shared metadata record for optional identification *)
+type metadata = {
+  id   : string;
+  name : string;
+}
+
 type material_metrics = {
   hs_code : Hs_code.t;
-  origin  : string; (** ISO 3166-1 alpha-2 country code of origin *)
+  origin  : string; (** ISO 3166-1 alpha-3 country code of origin *)
   cost    : float;  (** Unit cost, USD, non-negative *)
 }
 
 type material = {
-  id            : string;
-  name          : string;
+  metadata      : metadata option;
   trade_metrics : material_metrics;
 }
 
 type product_metrics = {
   hs_code             : Hs_code.t;
   export_value        : float;  (** free-on-board value, USD, non-negative *)
-  origin_country      : string; (** ISO 3166-1 alpha-2 country code *)
-  destination_country : string; (** ISO 3166-1 alpha-2 country code *)
+  origin_country      : string; (** ISO 3166-1 alpha-3 country code (e.g., "VNM") *)
+  destination_country : string; (** ISO 3166-1 alpha-3 country code (e.g., "MEX") *)
   bill_of_materials   : material list;
 }
 
 type product = {
-  id            : string;
-  name          : string;
+  metadata      : metadata option;
   trade_metrics : product_metrics;
 }
