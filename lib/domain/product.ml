f99ad6caff440109570993adb9dc79af72716c8b
nguyenhoangan-monkey <an.h.nguyen@stonybrook.edu>
2026-05-21 16:27:18 -0400

    rewrite

--- lib/domain/product.ml
+++ lib/domain/product.ml
@@ -20,8 +20,8 @@ type material = {
 type product_metrics = {
   hs_code             : Hs_code.t;
   export_value        : float;  (** free-on-board value, USD, non-negative *)
-  origin_country      : string; (** ISO 3166-1 alpha-3 country code (e.g., "VNM") *)
-  destination_country : string; (** ISO 3166-1 alpha-3 country code (e.g., "MEX") *)
+  origin_country      : string; (** ISO 3166-1 alpha-3 country code *)
+  destination_country : string; (** ISO 3166-1 alpha-3 country code *)
   bill_of_materials   : material list;
 }
 
